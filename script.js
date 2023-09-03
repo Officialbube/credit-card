addf3d2db2e26125158f7090dd955816e5c153ff
officialbube <ebubenkereebube@gmail.com>
2023-09-03 22:24:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/*const form = document.getElementById('form')
+const nameInput = document.getElementById("nameInput");
+const numberLabel = document.querySelector('.number-label')
+
+console.log(numberLabel)
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs;
+});
+
+const checkInputs = () => {
+    const nameValue = nameInput.value
+    console.log(nameValue)
+}*/
+
+/*nameInput.addEventListener("keyup", (event) => {
+    
+const nameInput = document.getElementById("nameInput");
+const display = document.querySelector(".name");
+
+let nameInputValue = nameInput.value;
+display.innerHTML = nameInputValue
+
+    
+    //const value = nameInput.value;
+    
+    //display.textContent = value;
+    
+})*/
